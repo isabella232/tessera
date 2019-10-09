2fab819a8fb989d18d48c5052db2553f63b3bf81
namtruong <nam.p.truong@outlook.com>
2019-10-09 18:22:36 +0100

    transactional boundary incorrectly set

--- tessera-core/src/main/java/com/quorum/tessera/transaction/TransactionManagerImpl.java
+++ tessera-core/src/main/java/com/quorum/tessera/transaction/TransactionManagerImpl.java
@@ -194,7 +194,6 @@ public class TransactionManagerImpl implements TransactionManager {
     }
 
     @Override
-    @Transactional
     public ResendResponse resend(ResendRequest request) {
 
         final byte[] publicKeyData = base64Decoder.decode(request.getPublicKey());
